df75893f6f9e658f17eb6d7943dd5a947bfafa32
yasir ahamed syed <sonu.yasir@gmail.com>
2020-01-29 01:52:55 +0300

    Change specific to Atomic read and write to Master and Instrument statistics calculation.
    Change specific to Atomic read and write to Master and Instrument statistics calculation.

--- src/main/java/com/tick/tracker/statistics/TickStatisticsCalculator.java
+++ src/main/java/com/tick/tracker/statistics/TickStatisticsCalculator.java
@@ -1,60 +1,97 @@
 package com.tick.tracker.statistics;
 
-import java.util.Optional; 
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.concurrent.locks.StampedLock;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory; 
+import org.slf4j.LoggerFactory;
 
 import com.tick.tracker.MessageDTO.BasicTick;
 import com.tick.tracker.MessageDTO.StatisticsCalculatorInterface;
 import com.tick.tracker.MessageDTO.StatisticsInterface;
 import com.tick.tracker.MessageDTO.Status;
+
 /***
  * 
- * This is the main Statistics Calculator holds 2 objects
- *   1. The master Statistics object that serves to GET  /statistics requests and holds the master statistics
- *   2. Map of instruments that hold individual TickStatisticsMaps.
+ * This is the main Statistics Calculator holds 2 objects 1. The master
+ * Statistics object that serves to GET /statistics requests and holds the
+ * master statistics 2. Map of instruments that hold individual
+ * TickStatisticsMaps.
  * 
  * 
  * @author Yasir <sonu.yasir@gmail.com>
  *
  */
-public class TickStatisticsCalculator implements StatisticsCalculatorInterface{
-	
-    private final static Logger log=LoggerFactory.getLogger("com.tick.tracker.statistics.TickStatisticsCalculator"); 
+public class TickStatisticsCalculator implements StatisticsCalculatorInterface {
+
+	private final static Logger log = LoggerFactory.getLogger("com.tick.tracker.statistics.TickStatisticsCalculator");
+
+	private final StampedLock lock = new StampedLock();
 
 	private final TickStatisticsDataStructure masterCalculator;
 	private final ConcurrentHashMap<String, TickStatisticsDataStructure> instrumentMap;
-	public TickStatisticsCalculator(TickStatisticsDataStructure masterCalculator, ConcurrentHashMap<String, TickStatisticsDataStructure> instrumentMap) {
+
+	public TickStatisticsCalculator(TickStatisticsDataStructure masterCalculator,
+			ConcurrentHashMap<String, TickStatisticsDataStructure> instrumentMap) {
 		super();
 		this.masterCalculator = masterCalculator;
 		this.instrumentMap = instrumentMap;
 	}
-	
-	private void add(final String instrument, final Double value) {	
-		TickStatisticsDataStructure tickStatisticsMap = instrumentMap.getOrDefault(instrument, new TickStatisticsDataStructure());
-		masterCalculator.checkin(value);
-		tickStatisticsMap.checkin(value);
-		instrumentMap.putIfAbsent(instrument, tickStatisticsMap);
-		
+
+	private void add(final String instrument, final Double value) {
+		long theLock = 0;
+		try {
+			theLock = lock.tryWriteLock(10, TimeUnit.MILLISECONDS);
+			TickStatisticsDataStructure tickStatisticsMap = instrumentMap.getOrDefault(instrument,
+					new TickStatisticsDataStructure());
+			masterCalculator.checkin(value);
+			tickStatisticsMap.checkin(value);
+			instrumentMap.putIfAbsent(instrument, tickStatisticsMap);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Got Exception ");
+		} finally {
+			if (theLock != 0)
+				lock.unlock(theLock);
+		}
 	}
-	
+
+	AtomicInteger count = new AtomicInteger(0);
+
 	private void remove(final String instrument, final Double value) {
-		masterCalculator.checkout(value);
-		Optional
-		.ofNullable(instrumentMap.getOrDefault(instrument, null))
-		.ifPresent(x->x.checkout(value));
+
+		long theLock = 0;
+		try {
+			theLock = lock.tryWriteLock(10, TimeUnit.MILLISECONDS);
+			masterCalculator.checkout(value);
+			Optional.ofNullable(instrumentMap.getOrDefault(instrument, null)).ifPresent(x -> x.checkout(value));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Got Exception ");
+		} finally {
+			if (theLock != 0)
+				lock.unlock(theLock);
+		}
+
 	}
 
 	@Override
 	public void consumeTick(final BasicTick tick) {
-		if(tick.getStatus() == Status.RECEIVED) {
+
+		if (tick.getStatus() == Status.RECEIVED) {
 			this.add(tick.getInstrument(), tick.getPrice());
-		}else {
+
+		} else {
+
 			this.remove(tick.getInstrument(), tick.getPrice());
 		}
-		
+
+		if (masterCalculator.getCount() % 1000 == 0)
+			this.checkHealth();
 	}
 
 	@Override
@@ -64,10 +101,23 @@ public class TickStatisticsCalculator implements StatisticsCalculatorInterface{
 
 	@Override
 	public StatisticsInterface getStatistics(String instrument) {
-		log.debug("checkHealth() : "+this.checkHealth()); 
+		log.debug("checkHealth() : " + this.checkHealth());
 		return instrumentMap.get(instrument);
 	}
+
 	public boolean checkHealth() {
-		return (instrumentMap.values().stream().mapToInt(e-> e.getCount()).sum() == masterCalculator.getCount());
+
+		long theLock = lock.readLock();
+
+		try {
+			int fullCount = instrumentMap.values().stream().mapToInt(e -> e.getCount()).sum();
+
+			if (!(fullCount == masterCalculator.getCount())) {
+				throw new RuntimeException("Exception found");
+			}
+			return true;
+		} finally {
+			lock.unlock(theLock);
+		}
 	}
 }
