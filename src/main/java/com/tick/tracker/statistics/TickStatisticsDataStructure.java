c62aba1f7d81943eea22fdf29212b1eecafddf78
yasir ahamed syed <sonu.yasir@gmail.com>
2020-01-29 01:54:06 +0300

    Changes specific to making Statistics (Master/instrument) as atomic.
    Changes specific to making Statistics (Master/instrument) as atomic.

--- src/main/java/com/tick/tracker/statistics/TickStatisticsDataStructure.java
+++ src/main/java/com/tick/tracker/statistics/TickStatisticsDataStructure.java
@@ -15,33 +15,42 @@ import com.tick.tracker.MessageDTO.StatisticsModel;
  * 
  * @TickStatisticsDataStructure is implemented on top of ConcurrentSkipListMap
  * 
- * 1. As Used ConcurrentSkipListMap as first() and last() has O(1) time complexity
+ *                              1. As Used ConcurrentSkipListMap as first() and
+ *                              last() has O(1) time complexity
  * 
- * 2. To have an optimal Memory usage I have used the following data structure:
- *       ConcurrentSkipListMap<Double , AtomicInteger >  
- *       where 
- *       - Double is the price  
- *       - AtomicInteger is the number of occurrence, it is a counter variable, utilizing the memory when the load is too high.
- *        
- * 3. This component only takes care of generating statistics. 
- * 4. This component only takes INSTRUMENT and PRICE, as expiry time is taken care by reactive_stream component
- * 5. If two prices are sent into this component for the same Instrument, instead of adding it in the map, it only increment its counter.
- *    
- * instead of saving every price in the store, I choose this  ConcurrentSkipListMap
+ *                              2. To have an optimal Memory usage I have used
+ *                              the following data structure:
+ *                              ConcurrentSkipListMap<Double , AtomicInteger >
+ *                              where - Double is the price - AtomicInteger is
+ *                              the number of occurrence, it is a counter
+ *                              variable, utilizing the memory when the load is
+ *                              too high.
  * 
- * example of data input :
- *         
- *         [ 1.2, 1.3, 1.1, 1.4, 1.1, 1.2, 1.4, 1.2, 1.1] 
+ *                              3. This component only takes care of generating
+ *                              statistics. 4. This component only takes
+ *                              INSTRUMENT and PRICE, as expiry time is taken
+ *                              care by reactive_stream component 5. If two
+ *                              prices are sent into this component for the same
+ *                              Instrument, instead of adding it in the map, it
+ *                              only increment its counter.
  * 
- * The above stream could be represented as follows in @TickStatisticsDataStructure :
+ *                              instead of saving every price in the store, I
+ *                              choose this ConcurrentSkipListMap
  * 
- *        { 1.1, 3}, { 1.2, 3}, { 1.3, 1}, { 1.4, 2}
+ *                              example of data input :
+ * 
+ *                              [ 1.2, 1.3, 1.1, 1.4, 1.1, 1.2, 1.4, 1.2, 1.1]
+ * 
+ *                              The above stream could be represented as follows
+ *                              in @TickStatisticsDataStructure :
+ * 
+ *                              { 1.1, 3}, { 1.2, 3}, { 1.3, 1}, { 1.4, 2}
  * 
  * 
  *
  */
 public class TickStatisticsDataStructure extends ConcurrentSkipListMap<Double, AtomicInteger>
-		implements StatisticsInterface, Serializable { 
+		implements StatisticsInterface, Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private AtomicReference<Double> sumOfActiveTicks;
@@ -50,44 +59,40 @@ public class TickStatisticsDataStructure extends ConcurrentSkipListMap<Double, A
 	 * 
 	 * 
 	 */
-	
+
 	private AtomicInteger doubleCounter;
-	
 
 	public TickStatisticsDataStructure() {
 		super();
 		sumOfActiveTicks = new AtomicReference<Double>(Double.valueOf(0.0));
-		doubleCounter=new AtomicInteger(0);
-	}
-
-	private double addPrice(double delta) {
-		while (true) {
-			Double currentValue = sumOfActiveTicks.get();
-			Double newValue = Double.valueOf(currentValue.doubleValue() + delta);
-			if (sumOfActiveTicks.compareAndSet(currentValue, newValue)) {
-				return currentValue.doubleValue();
-			}
-		}
-		
+		doubleCounter = new AtomicInteger(0);
 	}
 
 	public void checkin(Double value) {
-		addPrice(value);
-		doubleCounter.getAndIncrement();
-		AtomicInteger count = this.getOrDefault(value, new AtomicInteger(0));
-		this.putIfAbsent(value, count);
-		count.incrementAndGet();		
+
+		sumOfActiveTicks.getAndUpdate(newValue -> {
+			AtomicInteger count = this.getOrDefault(value, new AtomicInteger(0));
+			count.incrementAndGet();
+			this.putIfAbsent(value, count);
+			doubleCounter.getAndIncrement();
+			return newValue + value;
+
+		});
+
 	}
 
 	public void checkout(Double value) {
-		addPrice(-value);
-		doubleCounter.decrementAndGet();
-		this.computeIfPresent(value, (k, v) -> v.decrementAndGet() == 0 ? null : v);
-		}
+		sumOfActiveTicks.getAndUpdate(newValue -> {
+			doubleCounter.decrementAndGet();
+			this.computeIfPresent(value, (k, v) -> v.decrementAndGet() == 0 ? null : v);
+			return newValue - value;
+		});
+	}
 
 	public StatisticsModel getSnapshot() {
 		return new StatisticsModel(doubleCounter.get(), this.getMinPrice(), this.getMaxPrice(), this.getAverage());
-	} 
+	}
+
 	@Override
 	public int getCount() {
 		return doubleCounter.get();
